2cc7d6148fd85bef68bac1ba494eacd76a6f407c
TimurTimergalin <timur20041229@gmail.com>
2020-12-10 21:42:18 +0300

    pull

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -6,6 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def print_num():
     return "1"
+
+
+@app.route('/pull')
+def pull():
+    os.system("git pull")
+    return "OK"
 
 
 if __name__ == '__main__':
